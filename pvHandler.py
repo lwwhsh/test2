e993230e55f0794a6787e03e914aeece2fe3b2a0
root <lww@postech.ac.kr>
2015-11-20 22:32:05 +0900

    begin

--- pvHandler.py
+++ pvHandler.py
@@ -4,3 +4,10 @@ __author__ = 'root'
 import epics
 import scan
 import sys, time
+
+def monitorM3():
+    while True:
+        print epics.caget('mobiis:m3.RBV')
+        time.sleep(1)
+
+monitorM3()
