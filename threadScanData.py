c384569ec8228bd65f46a7bd9898f189ad191548
root <lww@postech.ac.kr>
2015-11-28 21:49:00 +0900

    added auto count mode change

--- threadScanData.py
+++ threadScanData.py
@@ -35,6 +35,7 @@ class ThreadScanData(QtCore.QThread, MakePointForScan):
             return
 
     def stop(self):
+        # 1st all scans ABORT!!
         self.client.abort()
         self.state = False
 
@@ -46,8 +47,8 @@ class ThreadScanData(QtCore.QThread, MakePointForScan):
 
     def run(self):
         self.state = True
-        self.last_log_fetched = None
-        self.last_logged = None
+        self.last_log_fetched = -1
+        self.last_logged = -1
 
         while not self.client.scanInfo(self.scan_id).isDone():
             if self.state is False:
@@ -59,6 +60,9 @@ class ThreadScanData(QtCore.QThread, MakePointForScan):
                 self.last_log_fetched = self.last_logged
                 self.commSignal.emit(self.client.scanInfo(self.scan_id).percentage())
 
+                self.data = self.client.getData(self.scan_id)
+                #print self.data
+
             QtCore.QThread.msleep(50) # time.sleep(0.01)
 
         try:
@@ -66,13 +70,18 @@ class ThreadScanData(QtCore.QThread, MakePointForScan):
         except:
             pass
 
-        # stop and return to e0Value and scaler set to auto mode.
-        epics.caput('HFXAFS:scaler1.CONT', 1)
-        epics.caput('mobiis:m2.STOP', 1.0)
-        time.sleep(1.0)
-        epics.caput('mobiis:m2.VAL', self.e0Value, wait=True, timeout=120)
-
+        # TODO: data save in here
         #print self.client.getData(self.scan_id)
         # self.scan_handler.clear()
+
+        self.post_scan()
+        self.state = False
+
+    def post_scan(self):
+        # here is post scan procedure
+        epics.caput('HFXAFS:scaler1.CONT', 1) # scaler set to auto mode
+        epics.caput('mobiis:m2.STOP', 1.0)    # DCM theta angle to e0 position
+        time.sleep(1.0)
+        epics.caput('mobiis:m2.VAL', self.e0Value, wait=True, timeout=120)
 
 # 우리나라 대한민국
